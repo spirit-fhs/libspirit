2c61252c7e1e19412168c3d2e01cea370eb13028
delbertooo <delbertooo@gmail.com>
2011-08-02 20:46:58 +0200

    installer works

--- src/libspirit.c
+++ src/libspirit.c
@@ -6,9 +6,15 @@
  */
 #include "libspirit.h"
 #include "libspiritconfig.h"
+#include "yajl/yajl_gen.h"
+#include "yajl/yajl_parse.h"
 
 
 LIBSPIRIT_API int fndlltest(void)
 {
+	yajl_gen g;
+	yajl_status st;
+	yajl_handle h;
+	st = yajl_parse(h, "[]", 3);
 	return 42;
 }
