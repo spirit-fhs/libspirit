3348938f812109aa281224452c95b96ec3a16f49
delbertooo <delbertooo@gmail.com>
2011-08-03 15:23:33 +0200

    included static libcurl into libspirit

--- src/libspirit.c
+++ src/libspirit.c
@@ -5,9 +5,10 @@
  *      Author: delbertooo
  */
 #include "libspirit.h"
-#include "libspiritconfig.h"
-#include "yajl/yajl_gen.h"
-#include "yajl/yajl_parse.h"
+#include <libspiritconfig.h>
+#include <yajl/yajl_gen.h>
+#include <yajl/yajl_parse.h>
+#include <curl/curl.h>
 
 
 LIBSPIRIT_API int fndlltest(void)
@@ -17,4 +18,21 @@ LIBSPIRIT_API int fndlltest(void)
 	yajl_handle h;
 	st = yajl_parse(h, "[]", 3);
 	return 42;
+}
+
+LIBSPIRIT_API int curltest(void)
+{
+	CURL *curl;
+	CURLcode res;
+
+//	curl = curl_easy_init();
+//	if(curl)
+//	{
+//		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+//		res = curl_easy_perform(curl);
+//
+//		/* always cleanup */
+//		curl_easy_cleanup(curl);
+//	}
+	return 0;
 }
