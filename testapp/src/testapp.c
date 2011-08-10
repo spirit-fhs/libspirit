7d9178fce971ef8fb959660c729ca8fb45830255
delbertooo <delbertooo@gmail.com>
2011-08-10 13:46:31 +0200

    now building a test application (testapp) during CMake build process

---
+++ testapp/src/testapp.c
@@ -0,0 +1,22 @@
+/*
+ ============================================================================
+ Name        : testapp.c
+ Author      : Robert Worgul
+ Version     :
+ Copyright   : 2011
+ Description : libspirit test application
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <libspirit/libspirit.h>
+
+int main(void) {
+	printf("-- libspirit test application --\n");
+	fflush(stdout);
+
+	curltest("https://212.201.64.226:8443/fhs-spirit/news");
+
+	return EXIT_SUCCESS;
+}
